8d63eb5a2d551d8c7fcceca73118ced17dbb3f73
unknown <tayloreldonjohnson@gmail.com>
2017-07-18 16:13:23 -0700

    js

---
+++ app.js
@@ -0,0 +1,13 @@
+function celsiusToFahrenheit(celsius) {
+  var celsiusInF = (celsius*9)/5 + 32;
+    getElementById("ceo");
+//   console.log(celsius + '°C is ' + celsiusInF + '°F');
+}
+
+function fahrenheitToCelsius(fahrenheit) {
+  var fahrenheitInC = ((fahrenheit - 32)*5)/9;
+  getElementById("fah");
+//   console.log(fahrenheit + '°F is ' + fahrenheitInC + '°C');
+}
+
+// fahrenheitToCelsius(50);
